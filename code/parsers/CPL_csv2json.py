1ee0871bb4fe2cb5185ac7e60b0988d085f56501
s8 <konstantin.leo@gmail.com>
2015-02-13 13:09:52 -0500

    arm pro mini parser added

--- code/parsers/CPL_csv2json.py
+++ code/parsers/CPL_csv2json.py
@@ -4,7 +4,6 @@ import cPickle as pickle
 # opening the file in the 'universal newline mode', hence the 'U' option
 octopartCPL_csv_bom = open('../../data/parts_lists/Common Parts Library BOM.csv', 'rU')
 
-
 def csv2dict_cpl(input_csv_file):
     """ Convert csv file into dictionary"""
     dict_from_csv = []
@@ -18,15 +17,17 @@ def csv2dict_cpl(input_csv_file):
                 entry['mpn'] = value
             if key == 'Manufacturer':
                 entry['brand'] = value
-            if key == 'Quantity':
-                continue
         dict_from_csv.append(entry)
     return dict_from_csv
 
-# cpl_bom = csv2dict_cpl(octopartCPL_csv_bom)
+cpl_bom = csv2dict_cpl(octopartCPL_csv_bom)
 
-# with open ('../../data/parts_lists/cpl_bom.json', 'wb') as fp:
-#     pickle.dump(cpl_bom, fp)
+with open ('../../data/parts_lists/cpl_bom.json', 'wb') as fp:
+    pickle.dump(cpl_bom, fp)
 
-# with open ('../../data/parts_lists/cpl_bom.json', 'rb') as fp:
-#     cpl_bom = pickle.load(fp)
+with open ('../../data/parts_lists/cpl_bom.json', 'rb') as fp:
+    cpl_bom = pickle.load(fp)
+
+for entry in cpl_bom:
+    print '------------------------------------'
+    print entry
--- code/parsers/armProMini_csv2json.py
+++ code/parsers/armProMini_csv2json.py
@@ -2,7 +2,8 @@ import csv
 import cPickle as pickle
 
 
-arduino_csv_bom = open('../../data/boms/arduino_bom.csv', 'r')
+arm_pro_mini_CSV_bom = open('../../data/boms/arm-pro-mini-bom.csv', 'rU')
+
 
 def csv2dict_arduino(input_csv_file):
     """ Convert csv file into dictionary"""
@@ -13,21 +14,21 @@ def csv2dict_arduino(input_csv_file):
         for item in line_item:
             key = str(item).strip()
             value = str(line_item[item]).strip()
-            if key == 'Part Number':
+            if key == 'Manufactuer Part number':
                 entry['mpn'] = value
-            if key == 'Manufacturer':
+            if key == 'Manufactuer Name':
                 entry['brand'] = value
         dict_from_csv.append(entry)
     return dict_from_csv
 
-arduino_bom = csv2dict_arduino(arduino_csv_bom)
+arm_pro_mini_bom = csv2dict_arduino(arm_pro_mini_CSV_bom)
 
-with open ('../../data/boms/arduino_bom.json', 'wb') as fp:
-    pickle.dump(arduino_bom, fp)
+with open('../../data/boms/arm_pro_mini_bom.json', 'wb') as fp:
+    pickle.dump(arm_pro_mini_bom, fp)
 
-with open ('../../data/boms/arduino_bom.json', 'rb') as fp:
-    arduino_bom = pickle.load(fp)
+with open('../../data/boms/arm_pro_mini_bom.json', 'rb') as fp:
+    arm_pro_mini_bom = pickle.load(fp)
 
-for entry in arduino_bom:
+for entry in arm_pro_mini_bom:
     print '------------------------------------'
     print entry
